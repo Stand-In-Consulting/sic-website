db2df8acf6a7c24bbe4f3c1fea4a272a8eafd79f
Eli Gibbs <eligibbs@outlook.com>
2025-08-08 21:45:33 -0400

    fix freshdesk

--- plugins/inject-chat-plugin.js
+++ plugins/inject-chat-plugin.js
@@ -10,24 +10,12 @@ export default function injectChatPlugin(context, options) {
             return {
                 // Injects tags before the closing </body> tag
                 postBodyTags: [
-                    // 1. Freshdesk inline script to set up widget settings
-                    {
-                        tagName: 'script',
-                        innerHTML: `
-              window.fwSettings={
-                'widget_id': 159000000382
-              };
-              !function(){if("function"!=typeof window.FreshworksWidget){var n=function(){n.q.push(arguments)};n.q=[],window.FreshworksWidget=n}}()
-            `,
-                    },
-                    // 2. Freshdesk external script, loaded with async and defer
+                    // The new, correct Freshworks widget script
                     {
                         tagName: 'script',
                         attributes: {
-                            type: 'text/javascript',
-                            src: 'https://widget.freshworks.com/widgets/159000000382.js',
-                            async: true,
-                            defer: true,
+                            src: '//fw-cdn.com/14008595/5883685.js',
+                            chat: 'true',
                         },
                     },
                 ],
